18d25bba6c9be0131f3b735cff6ebe24c5fad247
Joao Carlos <joaocmsabreu@gmail.com>
2019-01-25 11:13:03 -0300

    Ajuste no enpoint /Categorias/{id}(tratamento de erro)

--- src/main/java/com/joaocarlos/cursomc/services/CategoriaService.java
+++ src/main/java/com/joaocarlos/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.joaocarlos.cursomc.domain.Categoria;
 import com.joaocarlos.cursomc.repositories.CategoriaRepository;
+import com.joaocarlos.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,6 +18,7 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> cat = repository.findById(id);
 		
-		return cat.orElse(null);
+		return cat.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
