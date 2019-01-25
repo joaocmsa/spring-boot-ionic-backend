7d22d9ef21d456b164a123620c12ddca55810e31
Joao Carlos <joaocmsabreu@gmail.com>
2019-01-25 15:43:36 -0300

    Criaçao do service e resouce Cliente

---
+++ src/main/java/com/joaocarlos/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.joaocarlos.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaocarlos.cursomc.domain.Cliente;
+import com.joaocarlos.cursomc.repositories.ClienteRepository;
+import com.joaocarlos.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cat = repository.findById(id);
+		
+		return cat.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
